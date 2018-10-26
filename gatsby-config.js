fe66f2fd5e737729cf58d0f8f8b5adaaf03e65d3
andrew-secret <andre.rusakow@jimdo.com>
2018-10-26 19:12:14 +0200

    chore: configure gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,17 @@ module.exports = {
   siteMetadata: {
     name: `Hello Typescript World!`,
     tagline: `Gatsby + SASS + Typescript = 💪`
-  },  
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto\:300,400,500,700`
+        ]
+      }
+    }
   ],
 }
