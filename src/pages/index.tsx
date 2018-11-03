a0e143bdb90939cb75e6664f711c1bfe6a9088b5
andrew-secret <andre.rusakow@jimdo.com>
2018-11-03 23:32:48 +0100

    feat: provide content and include components to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,50 @@ import { Header } from '../components/header/header';
 import { Headline } from '../components/headline/headline';
 import { Button } from '../components/button/button';
 import { Paragraph } from '../components/paragraph/paragraph';
+import { SectionElement } from '../components/sectionElement/sectionElement';
+import { SectionWrapper } from '../components/sectionWrapper/sectionWrapper';
+import { IntroductionVisual } from '../components/introductionVisual/introductionVisual';
+import { StaffVisual } from '../components/staffVisual/staffVisual';
+import { FeatureList } from '../components/featureList/featureList';
+import { IFeatureListItemProps } from '../components/featureListItem/featureListItem';
 
 const styles = require('./Index.module.scss');
+
+const featureList: IFeatureListItemProps[] = [
+  {
+    label: 'Modular cable park solution'
+  },
+  {
+    label: 'Support available 7 days a week'
+  },
+  {
+    label: 'Automated processes and operations'
+  },
+  {
+    label: 'Automated invoicing'
+  },
+  {
+    label: 'Automated retargeting'
+  },
+  {
+    label: 'RFID access control'
+  },
+  {
+    label: 'Staff control'
+  },
+  {
+    label: 'Detailed statistics'
+  },
+  {
+    label: 'Customization'
+  }
+];
 
 export default class IndexPage extends React.Component {
   public render() {
     return (
       <BasicTemplate>
-        <Header>
+        <Header className={styles.test}>
           <Headline type="h1">Full-Size</Headline>
           <Headline className={styles.subhealine} type="h3">
             <span>wake</span>
@@ -23,9 +59,23 @@ export default class IndexPage extends React.Component {
           </Paragraph>
           <Button buttonSize="big" label="Request free demo" />
         </Header>
-        <div className={styles.Container}>
-          <h1>Moini</h1>
-        </div>
+        <SectionWrapper className={styles.sectionIntroduction}>
+          <div>
+            <Headline type="topic">discover our features</Headline>
+            <SectionElement headline="For the Owner" description="Control and streamline your operations" type="owner" />
+            <SectionElement headline="For your staff" description="Speed-up and optimize operations" type="staff" />
+            <SectionElement headline="For your customers" description="Let them do most operations themselves" type="customer" />
+          </div>
+          <IntroductionVisual />
+        </SectionWrapper>
+        <SectionWrapper className={styles.sectionOwner}>
+          <div className={styles.content}>
+            <Headline type="h2">For the owner</Headline>
+            <FeatureList featureList={featureList} />
+            <Button label="Get in touch" />
+          </div>
+          <StaffVisual />
+        </SectionWrapper>
       </BasicTemplate>
     );
   }
