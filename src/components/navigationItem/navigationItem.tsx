2b756c865c73393884ba830e841f58f8b7016eb0
andrew-secret <andre.rusakow@jimdo.com>
2018-11-03 23:16:57 +0100

    feat: add navigationItem

--- src/components/navigationItem/navigationItem.tsx
+++ src/components/navigationItem/navigationItem.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import * as classnames from 'classnames';
+
 import '../_constants.scss';
 
 const styles = require('./navigationItem.module.scss');
@@ -9,16 +11,17 @@ export interface INavigationItem {
   to: string;
   label: string;
   href?: string;
+  className?: string;
 }
 
-export const NavigationItem: React.SFC<INavigationItem> = ({ to, label, href }) => (
-  <li className={styles.navigationListItem}>
+export const NavigationItem: React.SFC<INavigationItem> = ({ to, label, href, className }) => (
+  <li className={classnames(styles.navigationListItem, className)}>
     {href ? (
       <a className={styles.navigationLinkItem} href={href}>
         {label}
       </a>
     ) : (
-      <Link className={styles.navigationLinkItem} to={to} activeStyle={{ color: constants.wakeSysBlue }}>
+      <Link className={styles.navigationLinkItem} to={to}>
         {label}
       </Link>
     )}
