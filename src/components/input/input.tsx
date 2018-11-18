382746af825f2f3d79866c2029e085a21c5073c4
andrew-secret <andre.rusakow@jimdo.com>
2018-11-18 11:47:21 +0100

    feat: add input component

---
+++ src/components/input/input.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import * as classnames from 'classnames';
+
+const styles = require('./input.module.scss');
+
+interface IInputProps {
+  htmlForLabel: string;
+  label: string;
+  placeholder?: string;
+  type: 'email' | 'url' | 'tel' | 'text';
+  autoFocus?: boolean;
+  value?: string;
+  className?: string;
+  inputClassName?: string;
+  onChange?: (event: React.SyntheticEvent<HTMLInputElement>) => void;
+  onKeyPress?: (event: React.SyntheticEvent<HTMLInputElement>) => void;
+}
+
+export const Input: React.SFC<IInputProps> = ({
+  htmlForLabel,
+  label,
+  placeholder,
+  type,
+  value,
+  autoFocus,
+  className,
+  inputClassName,
+  onChange,
+  onKeyPress
+}) => {
+  return (
+    <div className={classnames(styles.inputWrapper, className)}>
+      <label className={styles.inputLabel} htmlFor={htmlForLabel}>
+        {label}
+      </label>
+      <input
+        id={htmlForLabel}
+        type={type}
+        value={value}
+        name={htmlForLabel}
+        autoFocus={autoFocus}
+        className={classnames(styles.input, inputClassName)}
+        placeholder={placeholder}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+    </div>
+  );
+};
