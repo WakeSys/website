5de8214574dfa9a27f2db4813e16a81052353045
andrew-secret <andre.rusakow@jimdo.com>
2019-05-04 18:28:39 +0200

    feat(topBar): adjust refactored components

--- src/components/topBar/topBar.tsx
+++ src/components/topBar/topBar.tsx
@@ -15,10 +15,10 @@ interface ITopBarProps {
 export const TopBar: React.SFC<ITopBarProps> = ({ navigationList, label, openOverlay }) => {
   return (
     <div className={styles.topBar}>
-      <Logo type="colorful" />
+      <Logo type="white" />
       <div className={styles.wrapper}>
         <NavigationList navigationList={navigationList} />
-        <Button className={styles.topBarButton} buttonSize="small" label={label} onClick={openOverlay} />
+        <Button className={styles.topBarButton} buttonSize="small" label={label} onClick={openOverlay} buttonType="primary" />
       </div>
     </div>
   );
