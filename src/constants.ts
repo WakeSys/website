a30c0d17d93027d4d1d7774c4cdae59598fd186d
andrew-secret <andre.rusakow@jimdo.com>
2019-05-05 22:27:43 +0200

    feat(constants): add routes

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export enum Route {
+  ROOT = '/',
+  WAKEPARK = '/wake-park',
+  AQUAPARK = '/aqua-park',
+  SURFPARK = '/surf-park',
+  REFERENCES = '/references',
+  ABOUT_US = '/about',
+  TERMS_OF_SERVICE = '/terms',
+  PRIVACY_POLICY = '/policy',
+  IMPRINT = '/imprint'
+}
