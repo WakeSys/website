7dad13ab46b1bd51974e80aa72f85d4851ee3a9e
andrew-secret <andre.rusakow@jimdo.com>
2019-05-08 20:11:31 +0200

    feat(wake-park): adjust discover section on wakepark page

--- src/components/introductionVisual/introductionVisual.tsx
+++ src/components/introductionVisual/introductionVisual.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 
 const styles = require('./introductionVisual.module.scss');
-const introSatisticsVisual = require('../assets/introduction-statistics.png');
+const introSatisticsVisual = require('../assets/wakepark-intro-visual.png');
 
 export const IntroductionVisual: React.SFC = () => {
   return (
     <div className={styles.introductionVisual}>
-      <span className={styles.introductionBadge}>Trusted by 29 Full-size Parks.</span>
       <div className={styles.introductionWrapper}>
         <span className={styles.introductionTickets}>Tickets: $ 2000</span>
         <span className={styles.introductionMembership}>Memberships: $ 549,00</span>
