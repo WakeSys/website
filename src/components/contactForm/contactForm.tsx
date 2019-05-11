7e316ed70f3d8c2d336d5b3a0e0345c92b02dc13
andrew-secret <andre.rusakow@jimdo.com>
2019-05-11 15:17:32 +0200

    fix(contactForm): remove redirect

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -8,7 +8,7 @@ const styles = require('./contactForm.module.scss');
 export class ContactForm extends React.Component {
   public render() {
     return (
-      <form name="contact" method="POST" data-netlify="true" className={styles.contactForm} action="/success">
+      <form name="contact" method="POST" data-netlify="true" className={styles.contactForm}>
         <input type="hidden" name="form-name" value="contact" />
         <Headline className={styles.contactFormHeadline} type="h2">
           Request your free Wakesys demo
