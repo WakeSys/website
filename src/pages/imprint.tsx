dfc00ef24ac743c8bd4d13a2fc98e02800694758
andrew-secret <andre.rusakow@jimdo.com>
2019-05-14 19:07:10 +0200

    feat(imprint): add content

--- src/pages/imprint.tsx
+++ src/pages/imprint.tsx
@@ -1,15 +1,42 @@
 import * as React from 'react';
 import { BasicTemplate } from '../templates/basicTemplate';
-import { Header } from '../components/header/header';
+import { Header, HeaderType } from '../components/header/header';
 import { Headline } from '../components/headline/headline';
+import { SectionWrapper } from '../components/sectionWrapper/sectionWrapper';
+import { Paragraph } from '../components/paragraph/paragraph';
 
 export default class Imprint extends React.Component {
   public render() {
     return (
       <BasicTemplate>
-        <Header>
+        <Header type={HeaderType.GREY}>
           <Headline type="h1">Imprint</Headline>
         </Header>
+        <SectionWrapper alignment="column">
+          <Headline type="h2">Legal Address</Headline>
+          <Paragraph>
+            WakeSys s.a.r.l.
+            <br /> 3, montée St. Hubert
+            <br /> L8387 Koerich
+            <br /> Luxembourg
+          </Paragraph>
+        </SectionWrapper>
+        <SectionWrapper alignment="column">
+          <Headline type="h2">Contact information</Headline>
+          <Paragraph>
+            Phone: +49 151 2666 5395
+            <br /> Skype: wakesys
+            <br /> Email: info@wakesys.com
+          </Paragraph>
+        </SectionWrapper>
+        <SectionWrapper alignment="column">
+          <Headline type="h2">Other important stuff</Headline>
+          <Paragraph>
+            Business registration number: B181238
+            <br /> VAT number: LU26482247
+            <br /> Tax number: 2013 2447 951
+          </Paragraph>
+        </SectionWrapper>
       </BasicTemplate>
     );
   }
