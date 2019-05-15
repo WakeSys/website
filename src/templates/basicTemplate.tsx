481605b4e966e0eae8b332291aa6abb9ec5c6e19
andrew-secret <andre.rusakow@jimdo.com>
2019-05-15 19:10:44 +0200

    feat(basicTemplate): provide props for title and meta descriptions

--- src/templates/basicTemplate.tsx
+++ src/templates/basicTemplate.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
-
 import { TopBar } from '../components/topBar/topBar';
 import { Overlay } from '../components/overlay/overlay';
 import { Footer } from '../components/footer/footer';
@@ -10,12 +9,9 @@ import { Route } from '../constants';
 
 const styles = require('./basicTemplate.module.scss');
 
-interface IBasicTemplateProps {
-  children: React.ReactNode;
-}
-
-interface IBasicTemplateState {
-  isOverlayOpen: boolean;
+interface Props {
+  title?: string;
+  metaDescription?: string;
 }
 
 export const indexPageQuery = graphql`
@@ -44,36 +40,18 @@ const NAVIGATION_LIST = [
   }
 ];
 
-export class BasicTemplate extends React.Component<IBasicTemplateProps> {
-  public state: IBasicTemplateState = { isOverlayOpen: false };
+export const BasicTemplate: React.FunctionComponent<Props> = ({ children, title, metaDescription }) => {
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
 
-  public render() {
-    const { children } = this.props;
-    return (
-      <>
-        <Helmet
-          title="WakeSys Full-size - Online booking and management solutions for wakeboard cable parks"
-          meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
-        />
-        <TopBar navigationList={NAVIGATION_LIST} label="Free trial" openOverlay={this.openOverlay} />
-        {this.state.isOverlayOpen && (
-          <Overlay className={styles.basicTemplateOverlay} children={<ContactForm />} onCloseClick={this.closeOverlay} />
-        )}
-        {children}
-        <Footer />
-      </>
-    );
-  }
-
-  private closeOverlay = () => {
-    this.setState({
-      isOverlayOpen: false
-    });
-  };
-
-  private openOverlay = () => {
-    this.setState({
-      isOverlayOpen: true
-    });
-  };
-}
+  return (
+    <>
+      <Helmet title={title} meta={[{ name: 'description', content: metaDescription }]} />
+      <TopBar navigationList={NAVIGATION_LIST} label="Free trial" openOverlay={() => setIsOverlayOpen(true)} />
+      {isOverlayOpen && (
+        <Overlay className={styles.basicTemplateOverlay} children={<ContactForm />} onCloseClick={() => setIsOverlayOpen(false)} />
+      )}
+      {children}
+      <Footer />
+    </>
+  );
+};
