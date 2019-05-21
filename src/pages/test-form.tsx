29223406e5a10fba58446ba4ac1fb4ab351a6d4f
andrew-secret <andre.rusakow@jimdo.com>
2019-05-21 09:18:36 +0200

    fix: add ui input component

--- src/pages/test-form.tsx
+++ src/pages/test-form.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Input } from '../components/input/input';
 
 const TestForm: React.FunctionComponent = () => {
   return (
     <form name="test-contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="test-contact" />
-      <div className="field half first">
+      {/* <div className="field half first">
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" />
       </div>
@@ -16,7 +17,16 @@ const TestForm: React.FunctionComponent = () => {
       <div className="field">
         <label htmlFor="message">Message</label>
         <textarea name="message" id="message" />
-      </div>
+      </div> */}
+
+      <Input htmlForLabel="name" label="Name" type="text" placeholder="Type your name" name="name" />
+      <Input htmlForLabel="website" label="Your website" type="url" placeholder="Type your name" name="website" />
+      <Input htmlForLabel="email" label="E-Mail" type="email" placeholder="your@mail.com" name="email" />
+      <Input htmlForLabel="phone" label="Phone or Skype" type="text" name="phone" />
+      <textarea
+        name="message"
+        placeholder="Tell us a bit about your park. Is it Fullsize or Two-tower? Is it already open or when are you planning to open it?"
+      />
       <ul className="actions">
         <li>
           <input type="submit" value="Send Message" className="special" />
